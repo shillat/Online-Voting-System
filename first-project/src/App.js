a1e2503fa950a00b5a7773d20b24f83b2e3e71d2
shillat <shillahnaigaga5@gmail.com>
2025-10-18 18:23:49 +0300

    Updating dependencies after successful npm install

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -17,7 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+
         </Routes>
         <Footer /> {/* Footer always visible at bottom */}
       </div>
